2d7a31532698298bf21c2a6200cd8262d31b8e3d
GpNt <ff25tj@gmail.com>
2018-09-06 17:46:22 +0300

    some refactoring

--- vertxs/src/main/java/event_bus_test/MainVerticle.java
+++ vertxs/src/main/java/event_bus_test/MainVerticle.java
@@ -1,6 +1,7 @@
 package event_bus_test;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
@@ -12,22 +13,31 @@ public class MainVerticle extends AbstractVerticle implements Handler<Message<St
         Vertx vertx = Vertx.vertx();
         EventBus eventBus = vertx.eventBus();
 
-        MessageConsumer<String> consumer = eventBus.consumer("news.uk.sport");
+        MessageConsumer consumer = eventBus.consumer("news.uk.sport");
         consumer.handler(new MainVerticle());
 
-        eventBus.send("news.uk.sport", "Yay! Someone kicked a ball across a patch of grass", ar -> {
-            if (ar.succeeded()) {
-                System.out.println("Received reply: " + ar.result().body());
-            }
-        });
-        
-        long timerID = vertx.setPeriodic(1000, id -> {
-            System.out.println("And every second this is printed");
-        });
+        eventBus.send("news.uk.sport", "Yay! Someone kicked a ball across a patch of grass",
+                new Handler<AsyncResult<Message<Object>>>() {
+                    @Override
+                    public void handle(AsyncResult<Message<Object>> ar) {
+                        if (ar.succeeded()) {
+                            System.out.println("Received reply: " + ar.result().body());
+                        }
+                    }
+                });
+
+        long timerID = vertx.setPeriodic(1000,
+                new Handler<Long>() {
+                    @Override
+                    public void handle(Long id) {
+                        System.out.println("And every second this is printed");
+                    }
+                });
 
         System.out.println("First this is printed");
     }
-    public void handle(Message<String> message){
+
+    public void handle(Message message) {
         System.out.println("I have received a message: " + message.body());
         message.reply("how interesting!");
     }

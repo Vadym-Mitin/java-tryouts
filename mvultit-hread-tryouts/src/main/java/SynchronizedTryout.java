a9e40344171afd44aa55109da0de7af4a65ea708
GpNt <ff25tj@gmail.com>
2018-11-03 23:13:23 +0200

    add multi threads tryouts

---
+++ mvultit-hread-tryouts/src/main/java/SynchronizedTryout.java
@@ -0,0 +1,68 @@
+
+/**
+ * @author Vadym Mitin
+ */
+public class SynchronizedTryout {
+    private int counter;
+    private int notCount;
+
+    public synchronized int getCounter() {
+        return counter;
+    }
+
+    public synchronized void setCounter() {
+        this.counter++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        SynchronizedTryout tryout = new SynchronizedTryout();
+        tryout.sync();
+        Thread.sleep(2000);
+        tryout.notSync();
+    }
+
+    void notSync() throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 20000; i++) {
+                    notCount++;
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 20000; i++) {
+                    notCount++;
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("not sync = " + notCount);
+    }
+
+    void sync() throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 20000; i++) {
+                    setCounter();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 20000; i++) {
+                    setCounter();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("sync = " + getCounter());
+    }
+}
---
+++ mvultit-hread-tryouts/src/main/java/VolatileTryout.java
@@ -0,0 +1,54 @@
+import static java.lang.System.currentTimeMillis;
+
+/**
+ * @author Vadym Mitin
+ */
+public class VolatileTryout {
+    static int fixed = 0;
+    static int mobile = 0;
+
+    public static void main(String[] args) {
+        new Handler().start();
+        new Observer().start();
+
+    }
+
+    private static class Handler extends Thread {
+        @Override
+        public void run() {
+            while (fixed < 5) {
+                System.out.println("inc mobiled  = " + ++mobile);
+                System.out.println("inc fixed  = " + ++fixed);
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    private static class Observer extends Thread {
+        int a = fixed;
+        int b = mobile;
+        long l = currentTimeMillis();
+
+        @Override
+        public void run() {
+            while (b < 5) {
+
+                if ((currentTimeMillis() - l) > 8000L) break;
+
+                if (fixed != a) {
+                    System.out.println("fixed changed - " + fixed);
+                    a = fixed;
+                }
+                if (mobile != b) {
+                    System.out.println("mobile changed - " + mobile);
+                    b = mobile;
+                }
+            }
+        }
+    }
+}
